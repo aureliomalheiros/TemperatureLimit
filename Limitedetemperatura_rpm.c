d3276f42cbdb0d690badc3025c7ea25548e56890
Tribix <tribix@Tribix.tribix>
2016-11-03 10:43:56 -0300

    modificação

--- Limitedetemperatura_rpm.c
+++ Limitedetemperatura_rpm.c
@@ -1,6 +1,8 @@
-
 #include <16f873a.h>
 #define use_portb_lcd TRUE
+#include <lcd.c>
+#include <lcd.c>
+
 #device adc = 10
 #use delay(clock = 4M)
 #fuses xt,protect,put,brownout,lvp,nowdt
@@ -12,7 +14,7 @@
 #bit  bot_3 = portc.4
 #bit  buz = portc.1
 #bit  cg = portc.0
-#include <lcd.c>
+
 int1 x=1,scape;
 float temp;
 signed int tempdsl;
@@ -31,7 +33,7 @@ void main(){
    set_adc_channel(0);
    lcd_init();
    
-   WHILE(true){
+   while(true){
       
       lcd_putc("\f");
       analise=read_adc();
@@ -41,13 +43,13 @@ void main(){
       lcd_gotoxy(3,2);printf(lcd_putc "%.1f C",temp);
       delay_ms(500);
       tempdsl = read_eeprom(1);				//Leitura da eeprom
-      IF(read_eeprom(1)){
-       IF(temp < tempdsl ){
+      if(read_eeprom(1)){
+      	if(temp < tempdsl ){
          cg = 1;
       }
-      IF(temp > tempdsl){
+      if(temp > tempdsl){
          cg = 0;
-         FOR(c=0; c < 1;c++){
+         for(c=0; c < 1;c++){
             buz = 1;
             delay_ms(200);
             buz = 0;
@@ -56,15 +58,15 @@ void main(){
       }
       
       }
-      IF(bot_1){  
+      if(bot_1){  
          lcd_putc("\f");
          delay_ms(500);
          lcd_putc("CONTROLE");
          delay_ms(1000);
          scape=1;
          x = 1;
-         WHILE(scape==1){
-            IF(bot_1){
+         while(scape==1){
+            if(bot_1){
                c = 0;
                delay_ms(100);
                tempdsl = tempdsl + 1;
@@ -80,7 +82,7 @@ void main(){
                }
                
             }
-            IF(bot_2){
+            if(bot_2){
                c = 0;
                delay_ms(100);
                tempdsl = tempdsl - 1;
@@ -96,16 +98,16 @@ void main(){
                }
             }
             
-            IF(tempdsl < 0){
+            if(tempdsl < 0){
                tempdsl = -130;
             }
-            IF(tempdsl > 150){
+            if(tempdsl > 150){
                tempdsl = 0;
             }
             
             lcd_gotoxy(1,1);lcd_putc("TEMP. MAXIMA");
             lcd_gotoxy(1,2);printf(lcd_putc "%d C",tempdsl);
-            WHILE(scape == 0 && x == 1){
+            while(scape == 0 && x == 1){
                lcd_putc("\f");
                delay_ms(1000);
                lcd_putc("GRAVANDO...");
